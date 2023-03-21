7b8ac6256610e81b1ed9d35f9fca61eb28f3f467
AlekasndrHavryliuk <104818533+AlekasndrHavryliuk@users.noreply.github.com>
2023-03-22 01:07:51 +0200

    lazy loading

--- src/components/CardsGallery/CardsGallery.jsx
+++ src/components/CardsGallery/CardsGallery.jsx
@@ -1,8 +1,8 @@
-import Cards from 'components/Cards/Cards'
 import CustomContainer from 'components/Custom/CustomContainer/CustomContainer'
 import { MainTitle } from 'components/Custom/MainTitle/MainTitle.styled'
 import LoadMoreBtn from 'components/LoadMoreBtn/LoadMoreBtn'
-import { useEffect } from 'react'
+import { Suspense } from 'react'
+import { lazy, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { select } from 'store/selectors/selectors'
 import { getUsers } from 'store/usersReducer/usersOperations'
@@ -11,6 +11,7 @@ import { CustomCardsGallery, GallerySection } from './CardsGallery.styled'
 const CardsGallery = () => {
     const dispatch = useDispatch()
     const users = useSelector(select.users)
+    const LazyUserCard = lazy(() => import('../Cards/Cards'));
 
     useEffect(() => {
         dispatch(getUsers())
@@ -22,7 +23,9 @@ const CardsGallery = () => {
             <GallerySection>
                 <MainTitle>Working with GET request</MainTitle>
                 <CustomCardsGallery>
-                    {users ? users.map(user => <Cards key={user.id} user={user} />) : <h1>Waiting...</h1>}
+                    <Suspense    >
+                        {users ? users.map(user => <LazyUserCard key={user.id} user={user} />) : <h1>Waiting...</h1>}
+                    </Suspense>
                 </CustomCardsGallery>
                 <LoadMoreBtn />
             </GallerySection>
